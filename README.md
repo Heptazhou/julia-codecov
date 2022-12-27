f077b32652b109df99c6c8a1b0dcdb773738a202
Heptazhou <zhou@0h7z.com>
2022-12-27 21:17:52 +0000

    Update

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ###	Example
 
 ```yaml
+    steps:
       - uses: actions/checkout@v3
         with:
           persist-credentials: false
@@ -12,6 +13,32 @@
       - uses: julia-actions/julia-buildpkg@v1
       - uses: julia-actions/julia-runtest@v1
       - uses: heptazhou/julia-codecov@v1
+      - uses: codecov/codecov-action@v3
+        with:
+          file: lcov.info
+```
+
+OR
+
+```yaml
+    steps:
+      - uses: actions/checkout@v3
+        with:
+          persist-credentials: false
+      - uses: julia-actions/setup-julia@v1
+        with:
+          show-versioninfo: true
+      - uses: julia-actions/julia-buildpkg@v1
+      - uses: julia-actions/julia-runtest@v1
+      - uses: heptazhou/julia-codecov@v1
+        with:
+          skip: true
+          keep: false
+          file: lcov.info
+          dirs: |
+            src
+            dir1
+            dir2
       - uses: codecov/codecov-action@v3
         with:
           file: lcov.info
--- main.jl
+++ main.jl
@@ -1,22 +1,26 @@
+const bool(s::AbstractString) = parse(Bool, s)
+
 using Pkg
 
-Pkg.activate("coveragetempenv", shared = true)
+Pkg.activate("coverage_temp_env", shared = true)
 Pkg.add(PackageSpec(name = "CoverageTools"))
 
 using CoverageTools
 
 dirs = get(ENV, "INPUT_DIRECTORY", "src")
-lcov = get(ENV, "INPUT_FILE_LCOV", "lcov.info")
-ign = parse(Bool, get(ENV, "INPUT_FORCE", "false"))
+file = get(ENV, "INPUT_LCOV_FILE", "lcov.info")
+keep = get(ENV, "INPUT_LCOV_KEEP", "1") |> bool
+skip = get(ENV, "INPUT_SKIP_MISS", "0") |> bool
 
-dirs = filter!(!isempty, split(dirs, ":"))
+dirs = filter!(!isempty, split(dirs, r":|\n"))
+skip && filter!(ispath, dirs)
 for dir in dirs
 	isdir(dir) || error("`$dir` is not a directory")
 end
-isdir(lcov) && error("`$lcov` is a directory")
+isdir(file) && error("`$file` is a directory")
 
-fcs = !ign && isfile(lcov) ? LCOV.readfile(lcov) : vcat(process_folder.(dirs)...)
-LCOV.writefile(lcov, fcs)
+fcs = keep && isfile(file) ? LCOV.readfile(file) : vcat(process_folder.(dirs)...)
+LCOV.writefile(file, fcs)
 
 cvr, tot = get_summary(fcs)
 pct = round(100cvr / tot, digits = 2)
